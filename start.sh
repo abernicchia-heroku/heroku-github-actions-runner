9b963aa1e0e484ca606af0b82012a8ae8f3893f1
Andrea Bernicchia <abernicchia@salesforce.com>
2024-07-28 17:12:13 +0200

    echo on sigint/term that are duplicated

--- start.sh
+++ start.sh
@@ -74,8 +74,8 @@ attachRunner
 # SIGINT has a value of 2, so the exit code is 130 (128+2).
 # SIGTERM has a value of 15, so the exit code is 143 (128+15).
 # https://en.wikipedia.org/wiki/Signal_(IPC)#Default_action
-trap 'detachRunner; wait $PID' INT
-trap 'detachRunner; wait $PID' TERM
+trap 'echo SIGINT; detachRunner; wait $PID' INT
+trap 'echo SIGTERM; detachRunner; wait $PID' TERM
 
 # Normally, bash will ignore any signals while a child process is executing.
 # Starting the server with & (single ampersand) will background it into the
